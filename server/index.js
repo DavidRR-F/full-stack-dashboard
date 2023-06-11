afae9798554c77ec3bb38fca490e97899a13f54b
DavidRR-F <david.rosefranklin96@gmail.com>
2023-06-10 21:30:37 -0400

    dashboard

--- server/index.js
+++ server/index.js
@@ -19,12 +19,14 @@ import Product from "./models/Product.js"
 import ProductStat from "./models/ProductStat.js"
 import Transaction from "./models/Transaction.js"
 import OverallStat from "./models/OverallStat.js"
+import AffiliateStat from "./models/AffiliateStat.js"
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
   dataOverallStat,
+  dataAffiliateStat,
 } from "./data/index.js"
 
 dotenv.config()
@@ -58,6 +60,7 @@ mongoose
       // ProductStat.insertMany(dataProductStat)
       // Transaction.insertMany(dataTransaction)
       // OverallStat.insertMany(dataOverallStat)
+      // AffiliateStat.insertMany(dataAffiliateStat)
     })
   })
   .catch((err) => {
